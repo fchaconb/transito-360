8dcdbe2c7e16d4c9bfe9b98defb68b5647402e25
fchaconb <fchaconb@ucenfotec.ac.cr>
2024-10-22 19:01:06 -0600

    Solucion VS de Proyecto 2

---
+++ proyectoMultas/DTO/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class User
+    {
+        [Key]
+        public string Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public string ProfilePicture { get; set; }
+
+        public ICollection<Ticket> Tickets { get; set; }
+    }
+}
