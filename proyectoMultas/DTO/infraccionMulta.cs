0bcff3dd7770ed628b89655e3b77a34466ef5a4c
Nina2819 <cguitierrezc@ucenfotec.ac.cr>
2024-10-27 12:24:29 -0600

    Agrega infraccion multa

---
+++ proyectoMultas/DTO/infraccionMulta.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DTO
+{
+    public class infraccionMulta
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("CatalogoInfracciones")]
+        public int idInfraccion { get; set; }
+
+        [ForeignKey("Multas")]
+        public int idMulta { get; set; }
+    }
+}
