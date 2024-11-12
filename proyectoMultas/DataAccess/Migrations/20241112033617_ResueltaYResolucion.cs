a60ed34509eaccbf6ea66dce9fcecced86bed6df
fchaconb <fchaconb@ucenfotec.ac.cr>
2024-11-12 01:52:10 -0600

    Cambios en backend para administracion de disputas

---
+++ proyectoMultas/DataAccess/Migrations/20241112033617_ResueltaYResolucion.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class ResueltaYResolucion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "resuelta",
+                table: "Multas",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "resolucion",
+                table: "disputas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "resuelta",
+                table: "Multas");
+
+            migrationBuilder.DropColumn(
+                name: "resolucion",
+                table: "disputas");
+        }
+    }
+}
